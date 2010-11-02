9bc3958a61bfec7bac44f47c81da2ba78986b2fc
Hugo Frappier <hugo@machugo.godynamo.com>
2010-11-02 09:18:48 -0400

    Manquait :remember_me dans la liste des attributs mass-attribuables

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :lockable, :timeoutable and :activatable
   devise :registerable, :authenticatable,   #:confirmable, 
-    :recoverable,     :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, 
-    :firstname, :lastname, :title,:organization ,:city,:state,:country, :note, :is_admin 
+    :firstname, :lastname, :title,:organization ,:city,:state,:country, :note, :is_admin, :remember_me 
   
   def to_s
     [self.lastname, self.firstname ].join(", ")
